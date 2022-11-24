e420dedcb18151093183af0a2ba7a319051e07ce
Alessio Caradossi <acaradossi@hotels.com>
2022-11-24 17:29:57 +0100

    #20 adding converter and mediator for buttons, fixing unit test for weather

--- crac_server/service/weather_service.py
+++ crac_server/service/weather_service.py
@@ -1,14 +1,15 @@
 from crac_protobuf.chart_pb2_grpc import WeatherServicer
 from crac_protobuf.chart_pb2 import (
-    WeatherRequest,
-    WeatherResponse,
+    WeatherRequest,  # type: ignore
+    WeatherResponse,  # type: ignore
 )
-from crac_server.handler.weather_handler import WeatherHandler
+from crac_server.component.weather import WEATHER
+from crac_server.converter.weather_converter import WeatherConverter
 
 
 class WeatherService(WeatherServicer):
 
     def GetStatus(self, request: WeatherRequest, context) -> WeatherResponse:
-        weather_handler = WeatherHandler()
-        return weather_handler.handle(request)
+        weather_handler = WeatherConverter()
+        return weather_handler.convert(WEATHER)
 
