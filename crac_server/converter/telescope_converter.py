eaaf8697cc22d2bff575455c4082904ac1e7396b
Alessio Caradossi <acaradossi@hotels.com>
2022-11-26 00:30:56 +0100

    #20 added telescope handler, converter and mediator

---
+++ crac_server/converter/telescope_converter.py
@@ -0,0 +1,191 @@
+import logging
+from crac_protobuf.button_pb2 import (
+    ButtonKey,  # type: ignore
+    ButtonLabel,  # type: ignore
+    ButtonColor,  # type: ignore
+    ButtonGui,  # type: ignore
+)
+from crac_protobuf.telescope_pb2 import (
+    TelescopeAction,  # type: ignore
+    TelescopeResponse,  # type: ignore
+    TelescopeRequest,  # type: ignore
+    TelescopeSpeed,  # type: ignore
+    TelescopeStatus,  # type: ignore
+)
+from crac_server.component.telescope import (
+    TELESCOPE, 
+    Telescope,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class TelescopeMediator:
+    def __init__(self, request: TelescopeRequest) -> None:
+        self.request = request
+        self._action = request.action
+        self._button = TELESCOPE
+        self._status = self.button.status
+        self._speed = self.button.speed
+        self._connect_is_disabled = False
+        self._sync_is_disabled = False
+        self._park_is_disabled = False
+        self._flat_is_disabled = False
+    
+    
+    @property
+    def action(self) -> TelescopeAction:
+        return self._action
+
+    @property
+    def button(self) -> Telescope:
+        return self._button
+
+    @property
+    def status(self) -> TelescopeStatus:
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
+
+    @property
+    def speed(self) -> TelescopeSpeed:
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        self._speed = value
+
+    @property
+    def connect_is_disabled(self) -> bool:
+        return self._connect_is_disabled
+    
+    @connect_is_disabled.setter
+    def connect_is_disabled(self, value: bool):
+        self._connect_is_disabled = value
+
+    @property
+    def sync_is_disabled(self) -> bool:
+        return self._sync_is_disabled
+    
+    @sync_is_disabled.setter
+    def sync_is_disabled(self, value: bool):
+        self._sync_is_disabled = value
+
+    @property
+    def park_is_disabled(self) -> bool:
+        return self._park_is_disabled
+    
+    @park_is_disabled.setter
+    def park_is_disabled(self, value: bool):
+        self._park_is_disabled = value
+
+    @property
+    def flat_is_disabled(self) -> bool:
+        return self._flat_is_disabled
+    
+    @flat_is_disabled.setter
+    def flat_is_disabled(self, value: bool):
+        self._flat_is_disabled = value
+
+class TelescopeConverter:
+    def convert(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        
+        label_connect, metadata_connect = self.__text_metadata_connect_button(mediator.status)
+        connection_button_gui = ButtonGui(
+            key=ButtonKey.KEY_TELESCOPE_CONNECTION_TOGGLE,
+            label=label_connect,
+            metadata=metadata_connect,
+            button_color=self.__draw_connect_button(mediator.status),
+            is_disabled=mediator.connect_is_disabled
+        )
+
+        sync_button_gui = ButtonGui(
+            key=ButtonKey.KEY_SYNC,
+            label=ButtonLabel.LABEL_SYNC,
+            metadata=TelescopeAction.SYNC,
+            is_disabled=mediator.sync_is_disabled,
+            button_color=ButtonColor(text_color="black", background_color="white") if mediator.status < TelescopeStatus.LOST else ButtonColor(text_color="white", background_color="red") 
+        )
+
+        park_button_color, flat_button_color = self.__draw_park_flat_buttons(mediator.status)
+        park_button_gui = ButtonGui(
+            key=ButtonKey.KEY_PARK,
+            label=ButtonLabel.LABEL_PARK,
+            metadata=TelescopeAction.PARK_POSITION,
+            is_disabled=mediator.park_is_disabled,
+            button_color=park_button_color
+        ) 
+
+        flat_button_gui = ButtonGui(
+            key=ButtonKey.KEY_FLAT,
+            label=ButtonLabel.LABEL_FLAT,
+            metadata=TelescopeAction.FLAT_POSITION,
+            is_disabled=mediator.flat_is_disabled,
+            button_color=flat_button_color
+        )
+
+        return TelescopeResponse(
+            status=mediator.status, 
+            aa_coords=mediator.button.aa_coords, 
+            eq_coords=mediator.button.eq_coords,
+            speed=mediator.speed, 
+            buttons_gui=[
+                connection_button_gui,
+                sync_button_gui,
+                park_button_gui,
+                flat_button_gui,
+            ]
+        )
+
+    def __draw_connect_button(self, status):
+        if status < TelescopeStatus.LOST:
+            text_color = "white"
+            background_color = "green"
+        elif status in (TelescopeStatus.LOST, TelescopeStatus.DISCONNECTED):
+            text_color = "white"
+            background_color = "red"
+        else:
+            text_color = "white"
+            background_color = "orange"
+
+        return ButtonColor(text_color=text_color, background_color=background_color)
+    
+    def __text_metadata_connect_button(self, status):
+        if status < TelescopeStatus.LOST:
+            label = ButtonLabel.LABEL_TELESCOPE_CONNECTED
+            metadata = TelescopeAction.TELESCOPE_DISCONNECT
+        else:
+            label = ButtonLabel.LABEL_TELESCOPE_DISCONNECTED
+            metadata = TelescopeAction.TELESCOPE_CONNECT
+        return label, metadata
+
+    def __draw_park_flat_buttons(self, status):
+        if status is TelescopeStatus.PARKED:
+            park_button_color = ButtonColor(text_color="white", background_color="green")
+            flat_button_color = ButtonColor(text_color="black", background_color="white")
+        elif status is TelescopeStatus.FLATTER:
+            park_button_color = ButtonColor(text_color="black", background_color="white")
+            flat_button_color = ButtonColor(text_color="white", background_color="green")
+        elif status >= TelescopeStatus.LOST:
+            park_button_color = ButtonColor(text_color="white", background_color="red")
+            flat_button_color = ButtonColor(text_color="white", background_color="red")
+        else:
+            park_button_color = ButtonColor(text_color="black", background_color="white")
+            flat_button_color = ButtonColor(text_color="black", background_color="white")
+        return park_button_color, flat_button_color
+
+    # def __draw_park_flat_buttons310(self, status):
+    #     match status:
+    #         case TelescopeStatus.PARKED:
+    #             park_button_color = ButtonColor(text_color="white", background_color="green")
+    #             flat_button_color = ButtonColor(text_color="black", background_color="white")
+    #         case TelescopeStatus.FLATTER:
+    #             park_button_color = ButtonColor(text_color="black", background_color="white")
+    #             flat_button_color = ButtonColor(text_color="white", background_color="green")
+    #         case _:
+    #             park_button_color = ButtonColor(text_color="black", background_color="white")
+    #             flat_button_color = ButtonColor(text_color="black", background_color="white")
+    #     return park_button_color,flat_button_color
---
+++ crac_server/handler/telescope_handler.py
@@ -0,0 +1,111 @@
+import logging
+from crac_protobuf.button_pb2 import (
+    ButtonStatus,  # type: ignore
+)
+from crac_protobuf.telescope_pb2 import (
+    TelescopeAction,  # type: ignore
+    TelescopeResponse,  # type: ignore
+    TelescopeSpeed,  # type: ignore
+    TelescopeStatus,  # type: ignore
+)
+from crac_server.component.button_control import SWITCHES
+from crac_server.handler.handler import AbstractHandler
+from crac_server.converter.telescope_converter import TelescopeConverter, TelescopeMediator
+
+
+logger = logging.getLogger(__name__)
+
+
+class AbstractTelescopeHandler(AbstractHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if self._next_handler:
+            return self._next_handler.handle(mediator)
+        
+        return TelescopeConverter().convert(mediator)
+
+
+class TelescopeSwitchHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if SWITCHES["TELE_SWITCH"].get_status() is ButtonStatus.OFF:
+            mediator.connect_is_disabled = True
+            mediator.sync_is_disabled = True
+            mediator.park_is_disabled = True
+            mediator.flat_is_disabled = True
+            mediator.status = TelescopeStatus.LOST
+            mediator.speed = TelescopeSpeed.SPEED_ERROR
+            self._next_handler = None
+        
+        return super().handle(mediator)
+
+
+class TelescopeConnectHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.action is TelescopeAction.TELESCOPE_CONNECT:
+            mediator.button.polling_start()
+            self._next_handler = None
+        
+        return super().handle(mediator)
+
+
+class TelescopeDisconnectedHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.status is TelescopeStatus.DISCONNECTED or not mediator.button.polling:
+            mediator.speed=TelescopeSpeed.SPEED_ERROR
+            self._next_handler = None
+
+        return super().handle(mediator)
+
+
+class TelescopeDisconnectHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.action is TelescopeAction.TELESCOPE_DISCONNECT:
+            mediator.button.polling_end()
+            self._next_handler = None
+        
+        return super().handle(mediator)
+
+
+class TelescopeSyncHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.action is TelescopeAction.SYNC:
+            mediator.button.queue_sync(SWITCHES["TELE_SWITCH"].turned_on_at)
+        
+        return super().handle(mediator)
+
+
+class TelescopeParkHandler(AbstractTelescopeHandler):
+    def handle(self,  mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.action is TelescopeAction.PARK_POSITION:
+            mediator.button.queue_park()
+        
+        return super().handle(mediator)
+
+
+class TelescopeFlatHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.action is TelescopeAction.FLAT_POSITION:
+            mediator.button.queue_flat()
+        
+        return super().handle(mediator)
+
+
+class TelescopeFlatterHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if (
+                mediator.status is TelescopeStatus.FLATTER and 
+                SWITCHES["FLAT_LIGHT"].get_status() is ButtonStatus.OFF
+            ):
+            mediator.button.queue_set_speed(TelescopeSpeed.SPEED_NOT_TRACKING)
+
+        return super().handle(mediator)
+
+
+class TelescopeAutolightHandler(AbstractTelescopeHandler):
+    def handle(self, mediator: TelescopeMediator) -> TelescopeResponse:
+        if mediator.request.autolight:
+            if mediator.speed is TelescopeSpeed.SPEED_SLEWING:
+                SWITCHES["DOME_LIGHT"].on()
+            else:
+                SWITCHES["DOME_LIGHT"].off()
+
+        return super().handle(mediator)
