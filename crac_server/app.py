09c8054ae6c4793f72d48a2cb6acced3f9e1f1b9
Alessio Caradossi <acaradossi@hotels.com>
2023-09-28 21:33:10 +0200

    deleted streaming

--- crac_server/app.py
+++ crac_server/app.py
@@ -6,13 +6,11 @@ from crac_protobuf.chart_pb2_grpc import add_WeatherServicer_to_server
 from crac_protobuf.telescope_pb2_grpc import add_TelescopeServicer_to_server
 from crac_protobuf.roof_pb2_grpc import add_RoofServicer_to_server
 from crac_protobuf.curtains_pb2_grpc import add_CurtainServicer_to_server
-from crac_protobuf.camera_pb2_grpc import add_CameraServicer_to_server
 from crac_protobuf.button_pb2_grpc import add_ButtonServicer_to_server
 from crac_server.service.weather_service import WeatherService
 from crac_server.service.telescope_service import TelescopeService
 from crac_server.service.roof_service import RoofService
 from crac_server.service.curtains_service import CurtainsService
-from crac_server.service.camera_service import CameraService
 from crac_server.service.button_service import ButtonService
 from crac_server.config import Config
 from concurrent import futures
@@ -27,9 +25,6 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     add_ButtonServicer_to_server(
         ButtonService(), server
-    )
-    add_CameraServicer_to_server(
-        CameraService(), server
     )
     add_CurtainServicer_to_server(
         CurtainsService(), server
