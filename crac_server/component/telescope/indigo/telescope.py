2a4e4febd04f3aaaee56b23c60f2e6e0fee7e8fb
Raniero Albanesi <raniero.albanesi@gmail.com>
2024-03-19 11:30:34 +0100

    Update telescope.py
    replace UTF-8

--- crac_server/component/telescope/indigo/telescope.py
+++ crac_server/component/telescope/indigo/telescope.py
@@ -215,8 +215,8 @@ class Telescope(TelescopeBase):
             raise Exception(f"RA or Dec not present. RA: {ra}, DEC: {dec}")
 
     def __call(self, script: str):
-        self.s.sendall(script.encode('us-ascii'))
-        data = self.s.recv(30000).decode("us-ascii")
+        self.s.sendall(script.encode("UTF-8)
+        data = self.s.recv(30000).decode("UTF-8")
         logger.debug(f"data received from xml: {data}")
         try:
             return ET.fromstring(data)
