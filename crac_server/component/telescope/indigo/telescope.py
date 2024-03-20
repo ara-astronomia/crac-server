3968eff2aca4ffad41e3485ffea6fa73c15c503f
spider65 <raniero.albanesi@gmail.com>
2024-03-20 12:32:39 +0100

    add []

--- crac_server/component/telescope/indigo/telescope.py
+++ crac_server/component/telescope/indigo/telescope.py
@@ -142,11 +142,10 @@ class Telescope(TelescopeBase):
 
     def retrieve(self) -> str:
         root = self.__call(
-            f'''
-             {"getProperties": { "version": 512, "client": "{self._name}", "name": "MOUNT_EQUATORIAL_COORDINATES"} }
-            '''
-
-        )
+            f"""
+             [{"getProperties": { "version": 512, "client": "{self._name}", "name": "MOUNT_EQUATORIAL_COORDINATES"} }]
+            """
+            )
         eq_coords = self.__retrieve_eq_coords(root)
         speed = self.__retrieve_speed(root)
         aa_coords = self._retrieve_aa_coords(eq_coords)
