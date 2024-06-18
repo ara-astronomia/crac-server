190012d2995c0865d00f3348a8d4238907be0afa
spider65 <raniero.albanesi@gmail.com>
2024-06-18 19:34:59 +0200

    Modified name for XML

--- crac_server/component/telescope/indigo/telescope.py
+++ crac_server/component/telescope/indigo/telescope.py
@@ -8,6 +8,8 @@ from crac_server import config
 from crac_server.component.telescope.telescope import Telescope as TelescopeBase
 import logging
 import json
+import re
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -18,126 +20,49 @@ class Telescope(TelescopeBase):
     def __init__(self, hostname=config.Config.getValue("hostname", "telescope"), port=config.Config.getInt("port", "telescope")) -> None:
         super().__init__(hostname=hostname, port=port)
         self._name = config.Config.getValue("name", "indigo")
-        logger.debug(f"Mount type: {self._name}")
-        self.script = {"getProperties": { "version": 512, "client": "{self._name}", "name": "MOUNT_EQUATORIAL_COORDINATES"} }
-
-    def retrieve(self) -> tuple:
-        logger.debug(f"Mount typesssssss: {self._name}")
-        root = self.__call(script=self.script)
-        logger.debug(f"map indigo: {root}")
-        eq_coords = self.__retrieve_eq_coords(root)
-        speed = self.__retrieve_speed(root)
-        aa_coords = self._retrieve_aa_coords(eq_coords)
-        status = self._retrieve_status(aa_coords)
-        print(eq_coords, aa_coords, speed, status)
-        return (eq_coords, aa_coords, speed, status)
-
-
-    def __call(self, script: str, **kwargs) -> dict:
-        with open(script, 'r') as p:
-            file = p.read()
-            if kwargs:
-                if kwargs.get("az") is None:
-                    kwargs["az"] = ""
-                if kwargs.get("alt") is None:
-                    kwargs["alt"] = ""
-                file = file.format(**kwargs)
-            command = file.encode('utf-8')
-            logger.debug(f"Command sent su INDIGO: {command}")
-            self.s.sendall(command)
-
-        data = self.s.recv(1024).decode("utf-8")
-        error = self.__is_error__(data)
-        if error:
-            msg = f"Error code: {error}"
-            logger.error(msg)
-            raise ValueError(msg)
-        logger.debug(f"Data received from js: {data}")
-        jsonStringEnd = data.find("|")
-        jsonString = data[:jsonStringEnd]
-        try:
-            return json.loads(jsonString)
-        except json.decoder.JSONDecodeError as err:
-            logger.error(f"Json Malformed {err}")
-            raise err
-
-    def __parse_result(self, jsonLoad: dict) -> tuple:
-        #coords["error"] = self.__is_error__(jsonLoad)
-        logger.debug(f"json result is: {jsonLoad}")
-        #if not self.coords["error"]:
-        aa_coords = AltazimutalCoords(alt=round(jsonLoad["alt"], 2), az=round(jsonLoad["az"], 2))
-        if jsonLoad["tr"] == 1 and jsonLoad["sl"] == 1:
-            speed = TelescopeSpeed.SPEED_NOT_TRACKING 
-        elif jsonLoad["sl"] == 0:
-            speed = TelescopeSpeed.SPEED_SLEWING
-        elif jsonLoad["tr"] == 0:
-            speed = TelescopeSpeed.SPEED_TRACKING
-        else:
-            speed = TelescopeSpeed.SPEED_ERROR
-        
-        return (aa_coords, speed)
-        
-    def __is_error__(self, input_str, search_reg="Error = ([1-9][^\\d]|\\d{2,})") -> int:
-        r = re.search(search_reg, input_str)
-        error_code = 0
-        if r:
-            r2 = re.search('\\d+', r.group(1))
-            if r2:
-                error_code = int(r2.group(0))
-        return error_code
     
-    def sync():
-        pass
-    def flat():
-        pass
-    def set_speed():
-        pass
-    def park():
-        pass
-
-'''
     def sync(self, started_at: datetime):
         self.__call(
             f"""
-                <newSwitchVector client="{self._name}" name="MOUNT_ON_COORDINATES_SET">
+                <setSwitchVector device="{self._name}" name="MOUNT_ON_COORDINATES_SET">
                     <oneSwitch name="SLEW">
-                        OFF
+                        Off
                     </oneSwitch>
                     <oneSwitch name="TRACK">
-                        OFF
+                        Off
                     </oneSwitch>
                     <oneSwitch name="SYNC">
-                        ON
+                        On
                     </oneSwitch>
-                </newSwitchVector>
+                </setSwitchVector>
             """
         )
         eq_coords = self._calculate_eq_coords_of_park_position(started_at)
         self.__call(
             f"""
-                <newNumberVector client="{self._name}" name="MOUNT_EQUATORIAL_COORDINATES">
+                <defNumberVector device="{self._name}" name="MOUNT_EQUATORIAL_COORDINATES">
                     <oneNumber name="DEC">
                       {eq_coords.dec}
                     </oneNumber>
                     <oneNumber name="RA">
                       {eq_coords.ra}
                     </oneNumber>
-                </newNumberVector>
+                </defNumberVector>
             """
         )
         self.__call(
             f"""
-                <newSwitchVector client="{self._name}" name="MOUNT_ON_COORDINATES_SET">
+                <setSwitchVector device="{self._name}" name="MOUNT_ON_COORDINATES_SET">
                     <oneSwitch name="SLEW">
-                        OFF
+                        Off
                     </oneSwitch>
                     <oneSwitch name="TRACK">
-                        ON
+                        On
                     </oneSwitch>
                     <oneSwitch name="SYNC">
-                        OFF
+                        Off
                     </oneSwitch>
-                </newSwitchVector>
+                </setSwitchVector>
             """
         )
 
@@ -145,36 +70,36 @@ class Telescope(TelescopeBase):
         if speed is TelescopeSpeed.SPEED_NOT_TRACKING:
             self.__call(
                 f"""
-                    <newSwitchVector client="{self._name}" name="MOUNT_TRACKING">
+                    <setSwitchVector device="{self._name}" name="MOUNT_TRACKING">
                         <oneSwitch name="OFF">
-                            ON
+                            On
                         </oneSwitch>
-                    </newSwitchVector>
+                    </setSwitchVector>
                 """
             )
         else:
             self.__call(
                 f"""
-                    <newSwitchVector client="{self._name}" name="MOUNT_TRACKING">
+                    <setSwitchVector device="{self._name}" name="MOUNT_TRAKING">
                         <oneSwitch name="ON">
-                            ON
+                            On
                         </oneSwitch>
-                    </newSwitchVector>
+                    </setSwitchVector>
                 """
             )
             self.__call(
                 f"""
-                    <newSwitchVector client="{self._name}" name="MOUNT_ON_COORDINATES_SET">
+                    <setSwitchVector device="{self._name}" name="MOUNT_ON_COORDINATES_SET">
                         <oneSwitch name="SLEW">
-                            {"ON" if speed == TelescopeSpeed.SPEED_SLEWING else "OFF"}
+                            {"On" if speed == TelescopeSpeed.SPEED_SLEWING else "Off"}
                         </oneSwitch>
                         <oneSwitch name="TRACK">
-                            {"ON" if speed == TelescopeSpeed.SPEED_TRACKING else "OFF"}
+                            {"On" if speed == TelescopeSpeed.SPEED_TRACKING else "Off"}
                         </oneSwitch>
                         <oneSwitch name="SYNC">
-                            OFF
+                            Off
                         </oneSwitch>
-                    </newSwitchVector>
+                    </setSwitchVector>
                 """
             )
 
@@ -189,11 +114,11 @@ class Telescope(TelescopeBase):
         if speed is TelescopeSpeed.SPEED_NOT_TRACKING:
             self.__call(
                 f"""
-                <newSwitchVector client="{self._name}" name="MOUNT_TRACKING">
+                <setSwitchVector device="{self._name}" name="MOUNT_TRACKING">
                     <oneSwitch name="OFF">
-                        ON
+                        On
                     </oneSwitch>
-                </newSwitchVector>
+                </setSwitchVector>
                 """
             )
 
@@ -208,32 +133,35 @@ class Telescope(TelescopeBase):
         if speed is TelescopeSpeed.SPEED_NOT_TRACKING:
             self.__call(
                 f"""
-                <newSwitchVector client="{self._name}" name="MOUNT_TRACKING">
+                <setSwitchVector device="{self._name}" name="MOUNT_TRACKING">
                     <oneSwitch name="OFF">
-                        ON
+                        On
                     </oneSwitch>
-                </newSwitchVector>
+                </setSwitchVector>
                 """
             )
 
-    def retrieve(self) -> str:
+    def retrieve(self) -> tuple:
         root = self.__call(
-             {"getProperties": { "version": 512, "client": "{self._name}", "name": "MOUNT_EQUATORIAL_COORDINATES"} }
+            f"""
+            <getProperties device="{self._name}" version="1.7" name="MOUNT_EQUATORIAL_COORDINATES"/>
+            """
         )
         eq_coords = self.__retrieve_eq_coords(root)
         speed = self.__retrieve_speed(root)
         aa_coords = self._retrieve_aa_coords(eq_coords)
         status = self._retrieve_status(aa_coords)
+
         return (eq_coords, aa_coords, speed, status)
 
     def __move(self, aa_coords: AltazimutalCoords, speed=TelescopeSpeed.SPEED_TRACKING):
         self.__call(
             f"""
-                <newSwitchVector client="{self._name}" name="MOUNT_PARK">
+                <setSwitchVector device="{self._name}" name="MOUNT_PARK">
                     <oneSwitch name="UNPARKED">
-                        ON
+                        On
                     </oneSwitch>
-                </newSwitchVector>
+                </setSwitchVector>
             """
         )
         eq_coords = self._altaz2radec(aa_coords, decimal_places=2, obstime=datetime.utcnow()) if isinstance(aa_coords, (AltazimutalCoords)) else aa_coords
@@ -242,28 +170,37 @@ class Telescope(TelescopeBase):
         self.queue_set_speed(speed)
         self.__call(
             f"""
-                <newNumberVector client="{self._name}" name="MOUNT_EQUATORIAL_COORDINATES">
+                <defNumberVector device="{self._name}" name="MOUNT_EQUATORIAL_COORDINATES">
                     <oneNumber name="DEC">
                       {eq_coords.dec}
                     </oneNumber>
                     <oneNumber name="RA">
                       {eq_coords.ra}
                     </oneNumber>
-                </newNumberVector>
+                </defNumberVector>
             """
         )
 
     def __retrieve_speed(self, root):
         state = root.attrib["state"].strip() if root else None
-        if state == "OK":
+        if state == "Ok":
             return TelescopeSpeed.SPEED_TRACKING
-        elif state == "IDLE":
+        elif state == "Idle":
             return TelescopeSpeed.SPEED_NOT_TRACKING
-        elif state == "BUSY":
+        elif state == "Busy":
             return TelescopeSpeed.SPEED_SLEWING
         else:
             return TelescopeSpeed.SPEED_ERROR
-   
+        # match state:
+        #     case "Ok":
+        #         return TelescopeSpeed.SPEED_TRACKING
+        #     case "Idle":
+        #         return TelescopeSpeed.SPEED_NOT_TRACKING
+        #     case "Busy":
+        #         return TelescopeSpeed.SPEED_SLEWING
+        #     case _:
+        #         return TelescopeSpeed.SPEED_ERROR
+
     def __retrieve_eq_coords(self, root):
         ra, dec = None, None
         for coords in root.findall("defNumber"):
@@ -274,23 +211,15 @@ class Telescope(TelescopeBase):
         if ra and dec:
             return EquatorialCoords(ra=ra, dec=dec)
         else:
-            raise Exception(f"RA or DEC not present. RA: {ra}, DEC: {dec}")
+            raise Exception(f"RA or Dec not present. RA: {ra}, DEC: {dec}")
 
-    def __call(self, script):
-        data = script
-        properties=json.loads(data)
-        logger.debug(properties)
+    def __call(self, script: str):
+        self.s.sendall(script.encode('utf-8'))
+        data = self.s.recv(30000).decode("utf-8")
+        logger.debug(f"data received from xml: {data}")
         try:
-            return properties
-        except :
-            logger.error(f"properties not found")
+            return ET.fromstring(data)
+        except ET.ParseError as err:
+            logger.error(f"Xml Malformed {err}")
+            raise err
 
-'''
-
-        
-        #os.path.join(os.path.dirname(__file__), 'get_alt_az.js')
-        #self.script_move_track = os.path.join(os.path.dirname(__file__), 'set_move_track.js')
-        #self.script_sync_tele = os.path.join(os.path.dirname(__file__), 'sync_tele.js')
-        #self.script_disconnect_tele = os.path.join(os.path.dirname(__file__), 'disconnect_tele.js')
-
-    
