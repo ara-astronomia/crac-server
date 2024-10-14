bb62f459f2daef1ca721289af2e000a25592464a
spider65 <raniero.albanesi@gmail.com>
2024-10-14 19:01:18 +0200

    added round ,5

--- crac_server/component/telescope/telescope.py
+++ crac_server/component/telescope/telescope.py
@@ -277,7 +277,7 @@ class Telescope(ABC):
         azimuth =0 * u.deg
         altaz_frame=AltAz(alt=altezza, az=azimuth, location=observing_location,obstime=obstime)
         print(altaz_frame)
-        airmass = altaz_frame.secz #round((1 / np.sin(alt)), 5)
+        airmass = round((altaz_frame.secz), 5) #round((1 / np.sin(alt)), 5)
         print (f"questo è il valore di airmass calcolato: {airmass}")
         return Airmass(airmass=airmass)
     
