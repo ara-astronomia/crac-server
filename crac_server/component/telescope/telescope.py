6709123ddd862c85ca258292b38c904e048ac55b
spider65 <raniero.albanesi@gmail.com>
2024-10-14 16:00:14 +0200

    added secz for calculate airmass

--- crac_server/component/telescope/telescope.py
+++ crac_server/component/telescope/telescope.py
@@ -263,9 +263,8 @@ class Telescope(ABC):
 
     def _airmass (self, alt_az: AltazimutalCoords):
         alt=alt_az.alt
-        az = alt_az.az
-        print(az)
-        airmass= 1.34
+        print(alt)
+        airmass= alt.secz
         print (f"questo è il valore di airmass calcolato: {airmass}")
         return Airmass()
     
