4f7932d67613e6e28a0f8c08fd91409a67dc6571
spider65 <raniero.albanesi@gmail.com>
2024-10-18 12:40:07 +0200

    test

--- crac_server/component/telescope/indigo/telescope.py
+++ crac_server/component/telescope/indigo/telescope.py
@@ -239,7 +239,7 @@ class Telescope(TelescopeBase):
                         { 
                             "device": self._name, "name": "MOUNT_EQUATORIAL_COORDINATES", "state": "Ok", "items": 
                                 [
-                                    {"name": "RA", "value": eq_coords.ra, "name": "DEC", "value": eq_coords.dec} 
+                                    {"name": "DEC", "value": eq_coords.dec, "name": "RA", "value": eq_coords.ra} 
                                 ]
                         }
                     }
