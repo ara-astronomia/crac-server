dc0b96c35821cddead58031fcc61b449c557bde1
spider65 <raniero.albanesi@gmail.com>
2025-11-21 14:32:00 +0100

    apply conversion lan and lon to string from  float

--- crac_server/service/geographic_service.py
+++ crac_server/service/geographic_service.py
@@ -2,14 +2,25 @@
 from crac_protobuf import geographic_pb2
 from crac_protobuf import geographic_pb2_grpc
 from crac_server.config import Config # Supponendo che esista
+import re
 
 class GeographicServicer(geographic_pb2_grpc.GeographicServiceServicer):
     def GetGeographicInfo(self, request, context):
         # 1. Ottieni i dati dal config.ini
         geo_config = Config.get_section("geography")
-
-        lat = float(geo_config.get("lat", 0.0))
-        lon = float(geo_config.get("lon", 0.0))
+        lat=(geo_config.get("lat"))
+        lon=(geo_config.get("lon"))
+        elev=(geo_config.get("height"))
+        match_lat = re.search(r'(\d+)d([\d.]+)m', lat)
+        match_lon = re.search(r'(\d+)d([\d.]+)m', lon)
+        if match_lat and match_lon:
+            # Estrai i gruppi e convertili in float
+            gradi_lat  = float(match_lat(1))   # 42.0
+            minuti_lat = float(match_lat(2))  # 13.76
+            lat = float(gradi_lat + (minuti_lat / 60.0))  
+            gradi_lon  = float(match_lon(1))   # 42.0
+            minuti_lon = float(match_lon(2))  # 13.76
+            lat = float(gradi_lon + (minuti_lon / 60.0))   
         elev = float(geo_config.get("height", 0.0))
 
         # 2. Popola e restituisci il messaggio Protobuf
